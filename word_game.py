9333c9b5d56cdae6bef7e12814f83216edfe41e1
Daniel Berlyne <danberlyne@gmail.com>
2023-05-31 11:57:48 +0100

    Cleaned up code

--- word_game.py
+++ word_game.py
@@ -10,6 +10,18 @@ max_length = 13
 # Number of guesses per level
 guesses_per_level = 6
 
+# Introduction sequence
+def introduce_game():
+    print('=' * 50)
+    print('''Welcome to The Word Game.
+
+Guess the word correctly to proceed to the next level.
+A correct letter in the correct position will be displayed in lower case.
+A correct letter in the incorrect position will be displayed in upper case to the right of the game board.
+
+If you wish to quit at any point, type 'quit game'.''')
+    print('=' * 50 + '\n')
+
 # Returns `True` if `word` is a legal word
 def is_legal(word):
     return word.isalpha() and word.islower() and len(word) >= min_length and len(word) <= max_length
@@ -23,52 +35,15 @@ def get_words_by_size(word_list):
             words_by_size[len(word)].add(word)
     return words_by_size
 
-word_list_file = open('word_list.txt')
-word_list = word_list_file.readlines()
-word_list_file.close()
-words_by_size = get_words_by_size(word_list)
-
 # Picks a random word of a specified length
 def pick_random_word(words_by_size, length):
     return random.choice(list(words_by_size[length]))
 
-# Introduction sequence
-def introduce_game():
-    print('=' * 50)
-    print('''Welcome to The Word Game.
-
-    Guess the word correctly to proceed to the next level.
-    A correct letter in the correct position will be displayed in lower case.
-    A correct letter in the incorrect position will be displayed in upper case to the right of the game board.
-
-    If you wish to quit at any point, type 'quit game'.''')
-    print('=' * 50 + '\n')
-
-# Checks if player wishes to continue after completing all levels
-def check_to_continue():
-    while True:
-        print('Congratulations! You have completed all levels. Play again? (y/n)')
-        new_game = input()
-        if new_game == 'y':
-            break
-        elif new_game == 'n':
-            sys.exit()
-        else:
-            continue
-
-# Checks if player wishes to try again after a game over
-def check_to_try_again():
-    while True:
-        print(f'GAME OVER. The correct word was {word}. Try again? (y/n)')
-        new_game = input()
-        if new_game == 'y':
-            break
-        elif new_game == 'n':
-            sys.exit()
-        else:
-            continue
-
+# Main class for game logic
 class GameProcessor:
+    new_game = ''
+    remaining_guesses = guesses_per_level
+
     def __init__(self, length):
         self.length = length
         self.display = '%s ' * length
@@ -84,6 +59,13 @@ class GameProcessor:
     # Displays letters the player has revealed so far
     def display_game_board(self):
         print(self.display % tuple(self.revealed_letters))
+
+    def display_remaining_guesses(self):
+        print(f'{self.remaining_guesses} guesses remaining')
+
+    def display_game_state(self):
+        self.display_game_board()
+        self.display_remaining_guesses()
 
     # Tracks correctly guessed letters
     def update_revealed_letters(self, word, guess):
@@ -111,22 +93,54 @@ class GameProcessor:
         elif not guess.isalpha():
             print('Guess must contain only letters.')
 
+    # Player uses up a guess if their guess was a legal word
+    def update_remaining_guesses(self, guess):
+        if not self.is_illegal(guess):
+            self.remaining_guesses -= 1
+
+    # Checks if player wishes to continue after completing all levels
+    def check_to_continue(self):
+        while True:
+            print('Congratulations! You have completed all levels. Play again? (y/n)')
+            self.new_game = input()
+            if self.new_game == 'y':
+                break
+            elif self.new_game == 'n':
+                sys.exit()
+            else:
+                continue
+
+    # Reveals word and checks if player wishes to try again after a game over
+    def check_to_try_again(self, word):
+        while True:
+            print(f"GAME OVER. The correct word was '{word}'. Try again? (y/n)")
+            self.new_game = input()
+            if self.new_game == 'y':
+                break
+            elif self.new_game == 'n':
+                sys.exit()
+            else:
+                continue
+
+# Get word list from file and sort by length
+word_list_file = open('word_list.txt')
+word_list = word_list_file.readlines()
+word_list_file.close()
+words_by_size = get_words_by_size(word_list)
+
 # Main game loop
 while True:
-    new_game = ''
     introduce_game()
 
     # Ten levels with word lengths starting at 4 and increasing by 1 each level
     for length in range(min_length, max_length + 1):
         game = GameProcessor(length)
         word = pick_random_word(words_by_size, length)
-        remaining_guesses = guesses_per_level
         correct_guess = False
         game.display_level()
 
-        while remaining_guesses > 0:
-            game.display_game_board()
-            print(f'{remaining_guesses} guesses remaining')  
+        while game.remaining_guesses > 0:
+            game.display_game_state()
             guess = input().lower()
             if guess == 'quit game':
                 sys.exit()
@@ -135,21 +149,21 @@ while True:
                 break
             elif game.is_illegal(guess):
                 game.warn_player(guess)
-                continue
             else:
                 game.update_revealed_letters(word, guess)
-            remaining_guesses -= 1
-        
-        if correct_guess == True and length == 13:
-            check_to_continue()
+            game.update_remaining_guesses(guess)
+
+        # If player completes final level, asks if player wants to continue
+        if correct_guess == True and length == max_length:
+            game.check_to_continue()
         elif correct_guess == True:
             print('Congratulations! You guessed correctly. Proceed to the next level.')
         else:
             break
     
     # If player completed all levels and chose to play again, restarts main game loop
-    if new_game == 'y':
+    if game.new_game == 'y':
         continue
 
-    # If player uses up all guesses, proceeds to 'game over' sequence
-    check_to_try_again()
+    # If player used up all guesses, reveals word and asks if player wants to try again
+    game.check_to_try_again(word)
